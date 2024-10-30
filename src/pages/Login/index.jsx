0ea6146905bc13b5e2519fc6fea84a9bc46c90db
farshadmt <farshadkannapuram@gmail.com>
2024-10-30 11:44:01 +0530

    added animated login and signup tag

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,15 +3,18 @@ import './styles.css';
 
 
 const LoginForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [loginemail, setEmail] = useState('');
+  const [loginpassword, setPassword] = useState('');
+  const [signupemail, setSignupemail] = useState('');
+  const [signuppassword, setSignuppassword] = useState('');
+  const [signupcnpassword, setSignupcnpassword] = useState('');
   const [error, setError] = useState('');
   const [isLogin, setisLogin] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Basic validation
-    if (!email || !password) {
+    if (!loginemail || !loginpassword) {
       setError('Both fields are required.');
       return;
     }
@@ -19,7 +22,7 @@ const LoginForm = () => {
 .then(res => res.json())
 .then(console.log);
     // Here you would typically send a request to your API for authentication
-    console.log('Logging in with:', { email, password });
+    console.log('Logging in with:', { loginemail, loginpassword });
     
     // Reset fields
     setEmail('');
@@ -34,22 +37,22 @@ const LoginForm = () => {
 
   return (
     <div className="page-div">
-    <div className="login-form">
+    <div className={`login-form ${isLogin === 'login' ? 'active' : ''}`}>
       <div className="title-form">
-        <button className={`cartoon-form-button ${isLogin=='login' ? 'active' : ''}`} onClick={() => handleButtonClick('login')}>Login</button>    
-        <button className={`cartoon-form-button ${isLogin == 'signup' ? 'active' : ''}`} onClick={() => handleButtonClick('signup')}>Sign Up</button>
+        <button className={`cartoon-form-button ${isLogin === 'login' ? 'active' : ''}`} onClick={() => handleButtonClick('login')}>Login</button>    
+        <button className={`cartoon-form-button ${isLogin === 'signup' ? 'active' : ''}`} onClick={() => handleButtonClick('signup')}>Sign Up</button>
       </div> 
       <div className="title-name">
         <div className='cartoon-effect shaking-name'>Welcome to </div>
         <div className='cartoon-effect shaking-name'>BrainBuzzer</div>
       </div> 
       {error && <p style={{ color: 'red' }}>{error}</p>}
-      {isLogin == 'login'?
+      {isLogin === 'login'?
        <form onSubmit={handleSubmit} className='Formdiv'>
        <div className='field-container'>
          <label className='emailcartoon-effect moving-email'>Username:</label>
          <input 
-           value={email} 
+           value={loginemail} 
            onChange={(e) => setEmail(e.target.value)} 
            required 
            className='Input-style'
@@ -58,7 +61,7 @@ const LoginForm = () => {
        <div className='field-container'>
          <label className='emailcartoon-effect moving-password'>Password:</label>
          <input 
-           value={password} 
+           value={loginpassword} 
            onChange={(e) => setPassword(e.target.value)} 
            required 
            className='Input-style'
@@ -68,14 +71,17 @@ const LoginForm = () => {
        <div style={{alignItems:'center'}}>
          <button type="submit" className='cartoon-button'>Login</button>
        </div>
+       <div className='loged-user'onClick={() => handleButtonClick('signup')}>
+        <p className='loged-text'>he he... I already used this😂</p>
+       </div>
      </form>
       
       : <form onSubmit={handleSubmit} className='Formdiv'>
       <div className='field-container'>
         <label className='emailcartoon-effect moving-email'>User name:</label>
         <input 
-          value={email} 
-          onChange={(e) => setEmail(e.target.value)} 
+          value={signupemail} 
+          onChange={(e) => setSignupemail(e.target.value)} 
           required 
           className='Input-style'
         />
@@ -83,8 +89,8 @@ const LoginForm = () => {
       <div className='field-container'>
         <label className='emailcartoon-effect moving-password'>Password:</label>
         <input 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
+          value={signuppassword} 
+          onChange={(e) => setSignuppassword(e.target.value)} 
           required 
           className='Input-style'
           type='password'
@@ -93,8 +99,8 @@ const LoginForm = () => {
       <div className='field-container'>
         <label className='emailcartoon-effect moving-cnfrmpassword'>Confirm Password:</label>
         <input 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
+          value={signupcnpassword} 
+          onChange={(e) => setSignupcnpassword(e.target.value)} 
           required 
           className='Input-style'
           type='password'
